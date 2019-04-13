9a69595559d0959875a5301515d703318869ada9
crmmc <1879312087@qq.com>
2019-04-13 21:41:22 +0800

    add MultiThread Mode!

--- Ncm2Music.py
+++ Ncm2Music.py
@@ -22,14 +22,17 @@ print ('')
 print ('[MAIN]Importing Librarys')
 import binascii
 import struct
-import shutil
 import base64
 import json
+import time
+import threading
 import re
 import os
 import sys
 import glob
 import requests
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 import mutagen.id3
 from Crypto.Cipher import AES
 from mutagen.mp3 import MP3, EasyMP3 
@@ -37,8 +40,7 @@ from mutagen.flac import FLAC
 from mutagen.flac import Picture
 from mutagen.easyid3 import EasyID3
 from mutagen.id3 import COMM,APIC,ID3
-errnum = 0
-if sys.version_info < (3, 0):
+if sys.version_info < (3, 4):
     print ('[MAIN]Running in Python2')
     print ('[MAIN]reset system encoding to utf-8')
     reload(sys)
@@ -54,18 +56,11 @@ def Getlrc(neteaseID):
 
 
 
-def dump(file_path,nm1,nm2):
-    global errnum
-    errnum = 1
+def dump(file_path,Thnom):
     core_key = binascii.a2b_hex("687A4852416D736F356B496E62617857")
     meta_key = binascii.a2b_hex("2331346C6A6B5F215C5D2630553C2728")
     unpad = lambda s : s[0:-(s[-1] if type(s[-1]) == int else ord(s[-1]))]
-    try:
-    	f = open(file_path,'rb')
-    except:
-    	print ('Open File [' + file_path +"] Error! please try to  Delete special characters hit by files!")
-    	return -1
-    errnum =2
+    f = open(file_path,'rb')
     header = f.read(8)
     assert binascii.b2a_hex(header) == b'4354454e4644414d'
     f.seek(2, 1)
@@ -99,7 +94,7 @@ def dump(file_path,nm1,nm2):
     meta_data = bytes(meta_data_array)
     meta_data = base64.b64decode(meta_data[22:])
     cryptor = AES.new(meta_key, AES.MODE_ECB)
-    meta_data = unpad(cryptor.decrypt(meta_data)).decode('utf-8')[6:]
+    meta_data = unpad(cryptor.decrypt(meta_data))[6:]
     meta_data = json.loads(meta_data)
     crc32 = f.read(4)
     crc32 = struct.unpack('<I', bytes(crc32))[0]
@@ -107,17 +102,13 @@ def dump(file_path,nm1,nm2):
     image_size = f.read(4)
     image_size = struct.unpack('<I', bytes(image_size))[0]
     image_data = f.read(image_size)
-    file_name = meta_data['musicName'] + ' - ' + meta_data['artist'][0][0]
-    file_name = re.sub('[\/:*?"<>|].','-',file_name)
-    file_name = file_name.replace(" ",'-')
-    music_lrc = file_name
-    file_name = str((file_name + '.' + meta_data['format']).encode('utf-8').decode('utf-8'))
-    errnum =3
+    file_name = meta_data['musicName'] + ' - ' + meta_data['artist'][0][0] + '.' + meta_data['format']
+    file_name = str(file_name)
     if os.path.exists(file_name):
-    	print ('[+]Now [' + str(nm1) + '/' + str(nm2) + '][' + str(meta_data['format']) + ']['  + 'MusicID:' + str(meta_data['musicId']) + ']['+ str(meta_data['bitrate']/1000) + 'kbps] [' + str(file_path) +']>>>[' + str(file_name) + ']')
+    	print ('[+][Thread:{}]Now '.format(Thnom) + '[' + str(meta_data['format']) + ']['  + 'MusicID:' + str(meta_data['musicId']) + ']['+ str(meta_data['bitrate']/1000) + 'kbps] [' + str(file_path) +']>>>[' + str(file_name) + ']')
     	print ('[!]File is exist!')
     else:
-    	print ('[+]Now [' + str(nm1) + '/' + str(nm2) + '][' + str(meta_data['format']) + ']['  + 'MusicID:' + str(meta_data['musicId']) + ']['+ str(meta_data['bitrate']/1000) + 'kbps] [' + str(file_path) +']>>>[' + str(file_name) + ']')
+    	print ('[+][Thread:{}]Now '.format(Thnom) + '[' + str(meta_data['format']) + ']['  + 'MusicID:' + str(meta_data['musicId']) + ']['+ str(meta_data['bitrate']/1000) + 'kbps] [' + str(file_path) +']>>>[' + str(file_name) + ']')
     	m = open((os.path.join(os.path.split(file_path)[0],file_name)),'wb')
     	chunk = bytearray()
     	while True:
@@ -131,15 +122,15 @@ def dump(file_path,nm1,nm2):
         	m.write(chunk)
     	m.close()
     	f.close()
-    errnum =4
     picdata = ''
     music_id = meta_data['musicId']
     music_name = meta_data['musicName']
+    music_lrc = meta_data['musicName'] + ' - ' + meta_data['artist'][0][0]
     try:
     	picurl = meta_data['albumPic']
     	picdata = requests.get(picurl).content
     except:
-    	print ('[!]Get Picture From Internet Error!')
+    	print ('[!][Thread:{}]Get Picture From Internet Error!'.format(Thnom))
     if meta_data['format'] == 'mp3':
     	mp3_info = MP3(file_name, ID3=EasyID3)
     	#mp3_info.ID3.update_to_v23()
@@ -148,34 +139,29 @@ def dump(file_path,nm1,nm2):
     	mp3_info['album'] = meta_data['album']
     	mp3_info['artist'] = meta_data['artist'][0][0]
     	mp3_info['title'] = meta_data['musicName']
-    	mp3_info['discsubtitle'] = meta_data['alias']
-    	try:
-    		mp3_info['lyricist'] = str(arhhc['lrc']['lyric'].encode('utf-8'))
-    	except:
-    		hsvsbs = 'hshs'
+    	mp3_info['discsubtitle'] = meta_data['alias'] 
+    	mp3_info['lyricist'] = str(arhhc['lrc']['lyric'])
     	mp3_info.save()
     	hh = ID3(file_name)
     	hh.update_to_v23()
     	hh.save()
     	hh['APIC:'] = (APIC(mime='image/jpg',  data=picdata))
     	hh.save()
-    	errnum =5
-    	#print (music_lrc)
+    	#arhhc = Getlrc(music_id)
     	if 'lrc' in arhhc:
         	try:
         		f = open((music_lrc+'.lrc'),'w')
-        		f.write(arhhc['lrc']['lyric'])
+        		f.write(str(arhhc['lrc']['lyric']))
         		f.close()
         	except:
-        		print ('[!]LRC Get Error!')
+        		print ('[!][Thread:{}]LRC Get Error!'.format(Thnom))
     	if 'tlyric' in arhhc:
     		try:
     			l = open((music_lrc+'.tlyric'),'w')
-    			l.write(arhhc['tlyric']['lyric'])
+    			l.write(str(arhhc['tlyric']['lyric']))
     			l.close()
     		except:
-        		print ('[!]Tlyric Get Error!')
-    	errnum = 6
+        		print ('[!][Thread:{}]Tlyric Get Error!'.format(Thnom))
     elif meta_data['format'] == 'flac':
     	audio = FLAC(file_name)
     	audio["title"] = meta_data['musicName']
@@ -200,32 +186,57 @@ def dump(file_path,nm1,nm2):
     		if 'lrc' in arhhc:
         		try:
         			f = open((music_lrc+'.lrc'),'w')
-        			f.write(arhhc['lrc']['lyric'])
+        			f.write(str(arhhc['lrc']['lyric']))
         			f.close()
         		except:
-        			print ('[!]LRC Get Error!')
+        			print ('[!][Thread:{}]LRC Get Error!'.format(Thnom))
     		if 'tlyric' in arhhc:
     			try:
     				l = open((music_lrc+'.tlyric'),'w')
-    				l.write(arhhc['tlyric']['lyric'])
+    				l.write(str(arhhc['tlyric']['lyric']))
     				l.close()
     			except:
-        			print ('[!]Tlyric Get Error!')
+        			print ('[!][Thread:{}]Tlyric Get Error!'.format(Thnom))
     	except:
-    		print ('[!]FLAC Tags Save Error!')
+    		print ('[!]{}FLAC Tags Save Error!'.format(Thnom))
     		sfnr='title:'+meta_data['musicName']+'#$#' + 'artist:' + meta_data['artist'][0][0] +'#$#' +'album:'+ meta_data['musicName']+ '#$#albumPic:'+meta_data['albumPic']
-    		rew =open((file_name+'.songinfo').decode('utf-8'),'w')
+    		rew =open((file_name+'.songinfo'),'w')
     		rew.write(sfnr)
     		rew.close()
-    		print ('[!]Song Tags Has Been Saved On A SongInfo File!');
+    		print ('[!][Thread:{}]Song Tags Has Been Saved On A SongInfo File!'.format(Thnom));
     else:
-    		print ('[!]Unknow Type:'+meta_data['format'])
-    errnum = 0
-    return 0
+    		print ('[!][Thread:]{}Unknow Type:'.format(Thnom)+meta_data['format'])
+
+def MultiThreadChild(list,Number):
+	global dfgg
+	global totsong
+	for ncm1f in list:
+		print (Number)
+		print (ncm1f)
+		dump(ncm1f,Number)
+		try:
+			#dump(ncm1f,Number)
+			totsong = totsong + 1
+		except:
+			dfgg.write(ncm1f +'##File code Error!'+ "\n")
+			continue
+	print ('[+]Thread {} :Task Finish!'.format(Number))
+
 if __name__ == '__main__':
+	global AllTheardNumber
+	AllTheardNumber = 2
 	ncmfiles = glob.glob("*.ncm")
-	nm2 =len(ncmfiles)
-	nm1 = 0
+	if len(ncmfiles) < 1:
+		print ('[MAIN]NCM Files No Found!')
+		exit (0)
+	if len(ncmfiles) < AllTheardNumber:
+		AllTheardNumber = len(ncmfiles)
+	t=[0,]*AllTheardNumber
+	global dfgg
+	global totsong
+	global ncmlist
+	ncmlist = []
+	totsong = 0
 	print ('[MAIN]Running in a '+ sys.platform + ' system')
 	try:
 		dfgg = open('ncm2music_error.txt','a')
@@ -233,11 +244,8 @@ if __name__ == '__main__':
 		print ('[!]Open log file error!')
 	print ('[MAIN]There are ' + str(len(ncmfiles)) + ' Files ')
 	ttbig = 0
-	try:
-		for tii in ncmfiles:
-			ttbig = ttbig + os.path.getsize(tii)/1024/1024.0*1.6
-	except:
-		ttbig = -1
+	for tii in ncmfiles:
+		ttbig = ttbig + os.path.getsize(tii)/1024/1024.0*1.6
 	dw = 's'
 	if ttbig > 60:
 		ttbig = ttbig / 60.0
@@ -246,21 +254,30 @@ if __name__ == '__main__':
 			ttbig = ttbig / 60.0
 			dw = 'h'
 	print ('[MAIN]eta ' + str(round(ttbig,2)) + ' ' + dw)
+	print ('[MAIN]Open {} Threads To Convent This Files!'.format(AllTheardNumber))
+	last = int(len(ncmfiles) % AllTheardNumber)
+	avg = int((len(ncmfiles)-last)/AllTheardNumber)
+	for i in range(0,AllTheardNumber):
+		ncmlist.clear()
+		if i == (AllTheardNumber - 1):
+			for o in range(int(avg*i),int(avg * (i+1)+ last)):
+				ncmlist.append(ncmfiles[o])
+			#print (ncmlist)
+			t[i] =threading.Thread(target=MultiThreadChild,args=(ncmlist,i),daemon=True)
+			t[i].start()
+		else:
+			for o in range(int(avg*i),int(avg * (i+1) )):
+				ncmlist.append(ncmfiles[o])
+			#print (ncmlist)
+			t[i] =threading.Thread(target=MultiThreadChild,args=(ncmlist,i),daemon=True)
+			t[i].start()
 	print ('')
-	for ncmf in ncmfiles:
-		#dump(ncmf,nm1,nm2)
-		try:
-			nm1 = nm1 + 1
-			#print ('[MAIN] Task '+ str(nm1) + ' Running!')
-			dump(ncmf,nm1,nm2)
-			#print ('[MAIN] Task '+ str(nm1) + ' running Done!')
-		except:
-			dfgg.write(ncmf +'##File code Error!'+ "  ErrorCode:"+ str(errnum) + "\n")
-			fpath = "./ERRORFILES"
-			if not os.path.exists(fpath):
-				os.makedirs(fpath)
-				shutil.move(ncmf,fpath + "/" + ncmf)          
-				print ('move [ ' + ncmf + ' ]->[ ' + fpath + "/" + ncmf + ' ]')
-				continue
-	print("[MAIN]ALL Jobs Done!")
+	while(totsong < len(ncmfiles)):
+		print ('[MAIN]'+ '[' + time.asctime() +']Prograss: [' +  str(totsong) + '/' + str(len(ncmfiles)) + ']')
+		time.sleep(10)
+	print ('[MAIN]'+ '[' + time.asctime() +']Prograss: [' +  str(totsong) + '/' + str(len(ncmfiles)) + ']')
+	if os.path.getsize('ncm2music_error.txt') < 1:
+		os.remove('ncm2music_error.txt')
+	print ("[MAIN]ALL Jobs Done!")
 	dfgg.close()
+
